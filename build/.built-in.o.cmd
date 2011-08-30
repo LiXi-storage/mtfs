1377be726d40531043e17b00c043b8708bbe0aba
root <root@mdc62.(none)>
2011-08-30 19:36:40 +0800

    These is not sourcecode files.

--- build/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/hrfs/hrfs-1.5.31/build/built-in.o :=  rm -f /root/hrfs/hrfs-1.5.31/build/built-in.o; ar rcs /root/hrfs/hrfs-1.5.31/build/built-in.o
--- build/.conftest.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/hrfs/hrfs-1.5.31/build/conftest.ko := ld -m elf_x86_64 -r -o /root/hrfs/hrfs-1.5.31/build/conftest.ko /root/hrfs/hrfs-1.5.31/build/conftest.o /root/hrfs/hrfs-1.5.31/build/conftest.mod.o
