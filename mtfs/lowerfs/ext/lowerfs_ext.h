b0b795309a833a8e194c7a6c49cc28474a36f0e3
Li Xi <lixi@ddn.com>
2013-09-16 18:00:00 -0700

    Change __MTFS_REPLICA_EXT_H__ to __MTFS_LOWERFS_EXT_H__ in lowerfs_ext.h

--- mtfs/lowerfs/ext/lowerfs_ext.h
+++ mtfs/lowerfs/ext/lowerfs_ext.h
@@ -2,11 +2,11 @@
  * Copyright (C) 2011 Li Xi <pkuelelixi@gmail.com>
  */
 
-#ifndef __MTFS_REPLICA_EXT_H__
-#define __MTFS_REPLICA_EXT_H__
+#ifndef __MTFS_LOWERFS_EXT_H__
+#define __MTFS_LOWERFS_EXT_H__
 #if defined (__linux__) && defined(__KERNEL__)
 #include <linux/jbd.h>
 struct buffer_head *_mlowerfs_ext3_bread(handle_t *handle, struct inode *inode,
                                          int block, int create, int *err);
 #endif /* defined (__linux__) && defined(__KERNEL__) */
-#endif /* __MTFS_REPLICA_EXT_H__ */
+#endif /* __MTFS_LOWERFS_EXT_H__ */
